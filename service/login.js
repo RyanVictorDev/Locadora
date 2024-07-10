6bb2cc8028de1116df5cd5ebb37bb09c006841aa
RyanVictorDev <vryan8294@gmail.com>
2024-07-10 14:53:42 -0300

    teste

--- service/login.js
+++ service/login.js
@@ -1,38 +1,38 @@
-let apiClient;
+// let apiClient;
 
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.getElementById("loginForm");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const loginForm = document.getElementById("loginForm");
 
-    loginForm.addEventListener("submit", (e) => {
-        e.preventDefault();
+//     loginForm.addEventListener("submit", (e) => {
+//         e.preventDefault();
 
-        const username = document.getElementById("username").value.trim();
-        const password = document.getElementById("password").value.trim();
+//         const username = document.getElementById("username").value.trim();
+//         const password = document.getElementById("password").value.trim();
 
-        axios.post("https://livraria-api.altislabtech.com.br/auth/login", {
-            username: username,
-            password: password
-        })
-        .then(response => {
-            const token = response.data.token;
+//         axios.post("https://livraria-api.altislabtech.com.br/auth/login", {
+//             username: username,
+//             password: password
+//         })
+//         .then(response => {
+//             const token = response.data.token;
 
-            // Criar apiClient com o token recebido
-            apiClient = axios.create({
-                baseURL: "https://livraria-api.altislabtech.com.br",
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
+//             // Criar apiClient com o token recebido
+//             apiClient = axios.create({
+//                 baseURL: "https://livraria-api.altislabtech.com.br",
+//                 headers: {
+//                     'Authorization': `Bearer ${token}`
+//                 }
+//             });
 
-            // Salvando o token no localStorage para ser usado no cache  
-            localStorage.setItem('apiToken', token);
+//             // Salvando o token no localStorage para ser usado no cache  
+//             localStorage.setItem('apiToken', token);
 
-            // Redirecionando para a home
-            window.location.href = "../html/dashboard.html";
-        })
-        .catch(error => {
-            console.error('Erro ao fazer login:', error);
-            alert("Nome de usuário ou senha incorretos.");
-        });
-    });
-});
+//             // Redirecionando para a home
+//             window.location.href = "../html/dashboard.html";
+//         })
+//         .catch(error => {
+//             console.error('Erro ao fazer login:', error);
+//             alert("Nome de usuário ou senha incorretos.");
+//         });
+//     });
+// });
